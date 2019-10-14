54a1d4b9d362e24a5f580c578d62899d8ac6fb02
jjiojio jiojojoijio <helloantonhowareyou@outlook.com>
2019-10-14 11:48:18 +0200

    Finished

--- Editor/Form1.Designer.cs
+++ Editor/Form1.Designer.cs
@@ -33,14 +33,19 @@
             this.open = new System.Windows.Forms.ToolStripMenuItem();
             this.saveAs = new System.Windows.Forms.ToolStripMenuItem();
             this.close = new System.Windows.Forms.ToolStripMenuItem();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.extra = new System.Windows.Forms.ToolStripMenuItem();
             this.save = new System.Windows.Forms.ToolStripMenuItem();
             this.search = new System.Windows.Forms.ToolStripMenuItem();
             this.replace = new System.Windows.Forms.ToolStripMenuItem();
             this.undo = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.replaceTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lineLabel = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,7 +60,6 @@
             this.menuStrip1.Size = new System.Drawing.Size(800, 33);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "file";
-            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.MenuStrip1_ItemClicked);
             // 
             // file
             // 
@@ -70,23 +74,62 @@
             // open
             // 
             this.open.Name = "open";
-            this.open.Size = new System.Drawing.Size(270, 34);
+            this.open.Size = new System.Drawing.Size(199, 34);
             this.open.Text = "Öppna";
             this.open.Click += new System.EventHandler(this.Open_Click);
             // 
             // saveAs
             // 
             this.saveAs.Name = "saveAs";
-            this.saveAs.Size = new System.Drawing.Size(270, 34);
+            this.saveAs.Size = new System.Drawing.Size(199, 34);
             this.saveAs.Text = "Spara som";
             this.saveAs.Click += new System.EventHandler(this.SaveAs_Click);
             // 
             // close
             // 
             this.close.Name = "close";
-            this.close.Size = new System.Drawing.Size(270, 34);
+            this.close.Size = new System.Drawing.Size(199, 34);
             this.close.Text = "Avsluta";
             this.close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // extra
+            // 
+            this.extra.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.save,
+            this.search,
+            this.replace,
+            this.undo});
+            this.extra.Name = "extra";
+            this.extra.Size = new System.Drawing.Size(66, 29);
+            this.extra.Text = "Extra";
+            // 
+            // save
+            // 
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(174, 34);
+            this.save.Text = "Save";
+            this.save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // search
+            // 
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(174, 34);
+            this.search.Text = "Search";
+            this.search.Click += new System.EventHandler(this.Search_Click);
+            // 
+            // replace
+            // 
+            this.replace.Name = "replace";
+            this.replace.Size = new System.Drawing.Size(174, 34);
+            this.replace.Text = "Replace";
+            this.replace.Click += new System.EventHandler(this.Replace_Click);
+            // 
+            // undo
+            // 
+            this.undo.Name = "undo";
+            this.undo.Size = new System.Drawing.Size(174, 34);
+            this.undo.Text = "Undo";
+            this.undo.Click += new System.EventHandler(this.Undo_Click);
             // 
             // richTextBox1
             // 
@@ -103,55 +146,63 @@
             // saveFileDialog1
             // 
             this.saveFileDialog1.DefaultExt = "txt";
-            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.SaveFileDialog1_FileOk);
             // 
-            // extra
+            // searchTextBox
             // 
-            this.extra.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.save,
-            this.search,
-            this.replace,
-            this.undo});
-            this.extra.Name = "extra";
-            this.extra.Size = new System.Drawing.Size(66, 29);
-            this.extra.Text = "Extra";
+            this.searchTextBox.Location = new System.Drawing.Point(255, 7);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(100, 26);
+            this.searchTextBox.TabIndex = 2;
             // 
-            // save
+            // replaceTextBox
             // 
-            this.save.Name = "save";
-            this.save.Size = new System.Drawing.Size(270, 34);
-            this.save.Text = "Save";
-            this.save.Click += new System.EventHandler(this.Save_Click);
+            this.replaceTextBox.Location = new System.Drawing.Point(435, 7);
+            this.replaceTextBox.Name = "replaceTextBox";
+            this.replaceTextBox.Size = new System.Drawing.Size(100, 26);
+            this.replaceTextBox.TabIndex = 3;
             // 
-            // search
+            // label1
             // 
-            this.search.Name = "search";
-            this.search.Size = new System.Drawing.Size(270, 34);
-            this.search.Text = "Search";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Search";
             // 
-            // replace
+            // label2
             // 
-            this.replace.Name = "replace";
-            this.replace.Size = new System.Drawing.Size(270, 34);
-            this.replace.Text = "Replace";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(361, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Replace";
             // 
-            // undo
+            // lineLabel
             // 
-            this.undo.Name = "undo";
-            this.undo.Size = new System.Drawing.Size(270, 34);
-            this.undo.Text = "Undo";
+            this.lineLabel.AutoSize = true;
+            this.lineLabel.Location = new System.Drawing.Point(569, 10);
+            this.lineLabel.Name = "lineLabel";
+            this.lineLabel.Size = new System.Drawing.Size(72, 20);
+            this.lineLabel.TabIndex = 6;
+            this.lineLabel.Text = "lineLabel";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lineLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.replaceTextBox);
+            this.Controls.Add(this.searchTextBox);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Editor";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -174,6 +225,11 @@
         private System.Windows.Forms.ToolStripMenuItem search;
         private System.Windows.Forms.ToolStripMenuItem replace;
         private System.Windows.Forms.ToolStripMenuItem undo;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.TextBox replaceTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lineLabel;
     }
 }
 
--- Editor/Form1.cs
+++ Editor/Form1.cs
@@ -12,36 +12,28 @@ namespace Editor
 {
     public partial class Form1 : Form
     {
+        static string filename = ""; //File opened
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void MenuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
-        {
-
-        }
-
         private void Open_Click(object sender, EventArgs e)
         {
-            DialogResult dialogResult = openFileDialog1.ShowDialog();
-            if (dialogResult == DialogResult.OK)
+            DialogResult dialogResult = openFileDialog1.ShowDialog(); //Open file select menu
+            if (dialogResult == DialogResult.OK) //If file is selected 
             {
-                richTextBox1.Text = System.IO.File.ReadAllText(openFileDialog1.FileName);
+                filename = openFileDialog1.FileName; //Save current file into string for saving
+                richTextBox1.Text = System.IO.File.ReadAllText(openFileDialog1.FileName); //Fill textbox with content from file.
             }
         }
 
         private void SaveAs_Click(object sender, EventArgs e)
         {
-            DialogResult dialogResult = saveFileDialog1.ShowDialog();
-            if (dialogResult == DialogResult.OK)
+            DialogResult dialogResult = saveFileDialog1.ShowDialog(); //Open file select menu
+            if (dialogResult == DialogResult.OK) //If file is selected (file to save)
             {
-                System.IO.File.WriteAllText(saveFileDialog1.FileName, richTextBox1.Text);
+                System.IO.File.WriteAllText(saveFileDialog1.FileName, richTextBox1.Text); //Write all content from richtextbox into file.
             }
         }
 
@@ -52,12 +44,28 @@ namespace Editor
 
         private void Save_Click(object sender, EventArgs e)
         {
-
+                System.IO.File.WriteAllText(filename, richTextBox1.Text); //Save content from richtextbox to opened file
+        }
+        private void Search_Click(object sender, EventArgs e)
+        {
+            string searchQuery = searchTextBox.Text; //Save search query to string
+            int line = richTextBox1.GetLineFromCharIndex(
+                richTextBox1.Find(searchQuery)
+            ); //Find line that specific text locates at
+            lineLabel.Text = "Line " + line.ToString();
         }
 
-        private void SaveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        private void Replace_Click(object sender, EventArgs e)
         {
+            string searchQuery = searchTextBox.Text; //Save search query
+            string replaceQuery = replaceTextBox.Text; //Save replace query
+            richTextBox1.Text = richTextBox1.Text.Replace(searchQuery, replaceQuery); //Replace searchQuery content in richtextbox with replaceQuery content.
+            richTextBox1.Update();
+        }
 
+        private void Undo_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Undo(); //Undo text.
         }
     }
 }
