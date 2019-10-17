a2eb1225debc6e99a9d371dc274a81d706e9b878
jjiojio jiojojoijio <helloantonhowareyou@outlook.com>
2019-10-17 11:58:31 +0200

    Finished project

--- Editor/Form1.cs
+++ Editor/Form1.cs
@@ -17,7 +17,9 @@ namespace Editor
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// Opens file with FileDialog
+        /// </summary>
         private void Open_Click(object sender, EventArgs e)
         {
             DialogResult dialogResult = openFileDialog1.ShowDialog(); //Open file select menu
@@ -27,7 +29,9 @@ namespace Editor
                 richTextBox1.Text = System.IO.File.ReadAllText(openFileDialog1.FileName); //Fill textbox with content from file.
             }
         }
-
+        /// <summary>
+        /// Opens FileDialog and saves file
+        /// </summary>
         private void SaveAs_Click(object sender, EventArgs e)
         {
             DialogResult dialogResult = saveFileDialog1.ShowDialog(); //Open file select menu
@@ -36,16 +40,23 @@ namespace Editor
                 System.IO.File.WriteAllText(saveFileDialog1.FileName, richTextBox1.Text); //Write all content from richtextbox into file.
             }
         }
-
+        /// <summary>
+        /// Closes applciation
+        /// </summary>
         private void Close_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
-
+        /// <summary>
+        /// Saves open file 
+        /// </summary>
         private void Save_Click(object sender, EventArgs e)
         {
                 System.IO.File.WriteAllText(filename, richTextBox1.Text); //Save content from richtextbox to opened file
         }
+        /// <summary>
+        /// Searches for search query inside richtextbox
+        /// </summary>
         private void Search_Click(object sender, EventArgs e)
         {
             string searchQuery = searchTextBox.Text; //Save search query to string
@@ -54,7 +65,9 @@ namespace Editor
             ); //Find line that specific text locates at
             lineLabel.Text = "Line " + line.ToString();
         }
-
+        /// <summary>
+        /// Replaces searchQuery with replaceQuery inside of richTextBox
+        /// </summary>
         private void Replace_Click(object sender, EventArgs e)
         {
             string searchQuery = searchTextBox.Text; //Save search query
@@ -62,10 +75,12 @@ namespace Editor
             richTextBox1.Text = richTextBox1.Text.Replace(searchQuery, replaceQuery); //Replace searchQuery content in richtextbox with replaceQuery content.
             richTextBox1.Update();
         }
-
+        /// <summary>
+        /// Undos text
+        /// </summary>
         private void Undo_Click(object sender, EventArgs e)
         {
-            richTextBox1.Undo(); //Undo text.
+            richTextBox1.Undo(); //Undo text,  no need to reinvent the wheel.
         }
     }
 }
